41bca564444411db6fc859b01724ef6ad604640c
judeadaiy-dev <judeadaiy@gmail.com>
2026-05-14 14:21:37 +0300

    Delete tailwind.config.js

--- tailwind.config.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./App.js", "./screens/**/*.{js,jsx}", "./components/**/*.{js,jsx}"],
-  theme: { extend: {} },
-  plugins: [],
-}
