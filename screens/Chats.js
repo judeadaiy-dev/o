3b5ffd1c63cfe8f895fb9cd872b1c87f095847ac
judeadaiy-dev <judeadaiy@gmail.com>
2026-05-14 12:49:58 +0300

    Create Chats.js

---
+++ screens/Chats.js
@@ -0,0 +1,4 @@
+import { View, Text } from 'react-native';
+export default function ChatsScreen() {
+  return <View className="flex-1 bg-slate-950 justify-center items-center"><Text className="text-white text-xl">المحادثات الخاصة - قريباً</Text></View>;
+}
