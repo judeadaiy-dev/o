5faab8ed1dd0f7567be2f8dc1b5887ba4b60b1ae
judeadaiy-dev <judeadaiy@gmail.com>
2026-05-15 00:07:08 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+import 'react-native-gesture-handler'; // هذا لازم اول سطر اجباري
+import 'react-native-url-polyfill/auto';
 import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
-import 'react-native-url-polyfill/auto';
 
 AppRegistry.registerComponent(appName, () => App);
