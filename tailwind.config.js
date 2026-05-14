094d8e86ab719a0fcc2e7471b8bd29b6245e3067
judeadaiy-dev <judeadaiy@gmail.com>
2026-05-14 12:37:22 +0300

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,6 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./App.js", "./screens/**/*.{js,jsx}", "./components/**/*.{js,jsx}"],
+  theme: { extend: {} },
+  plugins: [],
+}
